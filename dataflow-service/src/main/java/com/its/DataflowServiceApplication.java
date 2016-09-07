72001ebf421e79140ac2496b715355f78fe45f9e
Dhaval Shah <dhaval201279@rediffmail.com>
2016-09-08 01:25:48 +0530

    Initial Commit

---
+++ dataflow-service/src/main/java/com/its/DataflowServiceApplication.java
@@ -0,0 +1,17 @@
+package com.its;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableDiscoveryClient
+@EnableDataFlowServer
+@SpringBootApplication
+public class DataflowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataflowServiceApplication.class, args);
+	}
+}
