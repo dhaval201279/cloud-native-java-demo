72ac73cb9ac1cab3882d954293480d2d946b162f
Dhaval Shah <dhaval201279@rediffmail.com>
2016-09-07 10:06:12 +0530

    Updated

--- reservation-service/src/main/java/com/its/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/its/ReservationServiceApplication.java
@@ -7,18 +7,71 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.SubscribableChannel;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableBinding(ReservationChannels.class)
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+}
+
+interface ReservationChannels {
+	@Input
+	SubscribableChannel input();
+}
+
+@MessageEndpoint
+class ReservationProcessor {
+	private final ReservationRepository reservationRepository;
+	
+	@Autowired
+	public ReservationProcessor(ReservationRepository reservationRepository) {
+		super();
+		this.reservationRepository = reservationRepository;
+	}
+
+
+
+	// String reservationName parameter can also be replaced with Spring Integraion's Message<String> message
+	@ServiceActivator(inputChannel = "input")
+	public void onNewReservation(String reservationName) {
+		this.reservationRepository.save(new Reservation(reservationName));
+	}
+}
+
+@RestController
+@RefreshScope
+class MessageRestController {
+	private final String value;
+
+	public MessageRestController(@Value("${message}") String value) {
+		super();
+		this.value = value;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value="/message")
+	String read() {
+		return this.value;
 	}
 }
 
